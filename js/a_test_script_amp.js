f51351890af49143ce0aacec19d568932d122632
bblacktetriss <58980483+Blacktetriss@users.noreply.github.com>
2021-02-19 23:50:29 +0200

    Update a_test_script_amp.js
    test amp doc script

--- js/a_test_script_amp.js
+++ js/a_test_script_amp.js
@@ -1,11 +1,19 @@
-var isScrollSnapSupported = 'scrollSnapType' in document.documentElement.style ||
-        'webkitScrollSnapType' in document.documentElement.style;
+// var isScrollSnapSupported = 'scrollSnapType' in document.documentElement.style ||
+//         'webkitScrollSnapType' in document.documentElement.style;
 
-if (!isScrollSnapSupported) {
-  var elem = document.createElement('p'),
-      txt  = document.createTextNode('Your browser does not support CSS Scroll Snap Points :( '),
-      local = document.body;
+// if (!isScrollSnapSupported) {
+//   var elem = document.createElement('p'),
+//       txt  = document.createTextNode('Your browser does not support CSS Scroll Snap Points :( '),
+//       local = document.body;
   
-  elem.appendChild(txt);
-  elem.classList.add('warning');
-  local.insertBefore(elem, local.firstChild);
+//   elem.appendChild(txt);
+//   elem.classList.add('warning');
+//   local.insertBefore(elem, local.firstChild);
+
+const button = document.getElementById('hello-inline');
+
+        button.addEventListener('click', () => {
+          const h1 = document.createElement('h1');
+          h1.textContent = 'Hello World!';
+          document.body.appendChild(h1);
+        });
