eaf3220894cd177609b4dce9ea5dc71d07594bd7
bblacktetriss <58980483+Blacktetriss@users.noreply.github.com>
2021-02-20 16:36:57 +0200

    Update a_test_script_amp.js

--- js/a_test_script_amp.js
+++ js/a_test_script_amp.js
@@ -1,13 +1,8 @@
-function myFunction() {
-    var x, text;
+const button = document.getElementById('hello-url');
 
-    x = document.getElementById("numb").value;
-
-    if (isNaN(x) || x < 1 || x > 10) {
-        text = "Input not valid";
-    } else {
-        text = "Input OK";
-    }
-    document.getElementById("demo").innerHTML = text;
-}
+button.addEventListener('click', () => {
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Hello FUCK World!';
+  document.body.appendChild(h1);
+});
 
