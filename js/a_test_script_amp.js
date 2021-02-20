ab7aa3b5aba3c5b0515efb803c35ccf940a61c54
bblacktetriss <58980483+Blacktetriss@users.noreply.github.com>
2021-02-20 16:58:18 +0200

    Update a_test_script_amp.js

--- js/a_test_script_amp.js
+++ js/a_test_script_amp.js
@@ -2,7 +2,15 @@ const button = document.getElementById('hello-url');
 
 button.addEventListener('click', () => {
   const h1 = document.createElement('h1');
-  h1.textContent = 'Hello FUCK 2 World!';
+  h1.textContent = 'Hello World!';
+  document.body.appendChild(h1);
+});
+
+const button = document.getElementById('hello-url2');
+
+button.addEventListener('click', () => {
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Hello 21 World!';
   document.body.appendChild(h1);
 });
 
