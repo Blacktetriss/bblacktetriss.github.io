ebbfca4fa92701bd65efe56787ba9f6136bd244a
bblacktetriss <58980483+Blacktetriss@users.noreply.github.com>
2021-02-24 09:13:59 +0200

    Update a_test_script_amp.js

--- js/a_test_script_amp.js
+++ js/a_test_script_amp.js
@@ -1,15 +1,15 @@
-const submit = document.getElementById('username-submit');
-        const input = document.getElementById('username-input');
-        const display = document.getElementById('username-display');
+// const submit = document.getElementById('username-submit');
+//         const input = document.getElementById('username-input');
+//         const display = document.getElementById('username-display');
 
-        const oldUsername = localStorage.getItem('username');
-        display.textContent = oldUsername ? oldUsername : '(not set)';
+//         const oldUsername = localStorage.getItem('username');
+//         display.textContent = oldUsername ? oldUsername : '(not set)';
 
-        function setUsername() {
-          const newUsername = input.value;
-          localStorage.setItem('username', newUsername);
-          display.textContent = newUsername;
-        }
+//         function setUsername() {
+//           const newUsername = input.value;
+//           localStorage.setItem('username', newUsername);
+//           display.textContent = newUsername;
+//         }
 
         submit.addEventListener('click', setUsername);
 
